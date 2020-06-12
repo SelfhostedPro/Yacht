9f3ed2d5edc11f126b672e8673060bd2795d24c0
SelfhostedPro <info@selfhosted.pro>
2020-06-12 12:51:47 -0700

    gitea repo checkpoint

--- app/__init__.py
+++ app/__init__.py
@@ -77,4 +77,7 @@ def create_app(config):
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
+    from .apps import apps as apps_blueprint
+    app.register_blueprint(apps_blueprint, url_prefix='/apps')
+
     return app
---
+++ app/apps/views.py
@@ -0,0 +1,40 @@
+from flask import (
+    Blueprint,
+    abort,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+from flask_login import current_user, login_required
+from flask_rq import get_queue
+
+from app import db
+from app.apps.forms import (
+    ChangeAccountTypeForm,
+    ChangeUserEmailForm,
+    InviteUserForm,
+    TemplateForm,
+)
+from app.decorators import admin_required
+from app.email import send_email
+from app.models import EditableHTML, Role, User
+
+apps = Blueprint('apps', __name__)
+
+
+@apps.route('/')
+@login_required
+@admin_required
+def index():
+    """Apps dashboard page."""
+    return render_template('apps/index.html')
+
+@apps.route('/new-template', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def new_template():
+    """Add a new app."""
+    form = TemplateForm()
+    return render_template('apps/new_template.html', form=form)
