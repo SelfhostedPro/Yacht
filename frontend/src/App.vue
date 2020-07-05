6aab8569a0b15c6555134ef75ae6dafe94d810b8
SelfhostedPro <info@selfhosted.pro>
2020-07-05 09:54:32 -0700

    Separated Nav into a component folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,73 +1,10 @@
 <template>
   <div id="app">
-    <b-navbar id="navbar" toggleable="lg" type="dark" variant="dark" v-if="isAuthenticated">
-      <b-navbar-brand href="#">
-        <img alt="Vue logo" src="./assets/logo.png" width="32" height="32" />
-      </b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <!--
-          <router-link to="/dashboard">Dashboard</router-link> |
-          -->
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item @click.native.prevent="logout">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <Navbar v-if="isAuthenticated"></Navbar>
     <b-container fluid>
       <b-row>
         <b-col cols="2" v-if="isAuthenticated">
-          <b-sidebar
-            id="sidebar-big"
-            shadow
-            no-header-close
-            no-close-on-esc
-            no-close-on-route-change
-          >
-            <template v-slot:footer>
-              <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
-                <b-button squared v-b-toggle="'sidebar-big sidebar-small'">
-                  <b-icon-arrow-bar-left></b-icon-arrow-bar-left>
-                </b-button>
-              </div>
-            </template>
-            <div class="sidebar-header">
-              <img alt="Vue logo" src="./assets/logo.png" width="32" height="32" />
-              <h3>Yacht</h3>
-              <p>Container Managment UI</p>
-            </div>
-          </b-sidebar>
-          <b-sidebar
-            id="sidebar-small"
-            width="5em"
-            shadow
-            no-header-close
-            no-close-on-esc
-            no-close-on-route-change
-            visible
-          >
-            <template v-slot:footer>
-              <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
-                <b-button squared v-b-toggle="'sidebar-big sidebar-small'">
-                  <b-icon-arrow-bar-right></b-icon-arrow-bar-right>
-                </b-button>
-              </div>
-            </template>
-            <div class="sidebar-header">
-              <img alt="Vue logo" src="./assets/logo.png" width="32" height="32" />
-            </div>
-          </b-sidebar>
+        <Sidebar></Sidebar>
         </b-col>
         <b-col>
           <router-view id="content" />
@@ -78,20 +15,18 @@
 </template>
 
 <script type="text/javascript">
+import Navbar from "./components/nav/Navbar"
+import Sidebar from "./components/nav/Sidebar"
 export default {
+  components: {
+    "Navbar" : Navbar,
+    "Sidebar" : Sidebar,
+  },
   computed: {
     isAuthenticated() {
       return this.$store.getters["auth/isAuthenticated"];
     }
   },
-  methods: {
-    logout() {
-      console.log("logout");
-      this.$store.dispatch("auth/logout").then(() => {
-        this.$router.push("/");
-      });
-    }
-  }
 };
 </script>
 
---
+++ frontend/src/components/nav/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-sidebar id="sidebar-big" shadow no-header-close no-close-on-esc no-close-on-route-change>
+      <template v-slot:footer>
+        <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
+          <b-button squared v-b-toggle="'sidebar-big sidebar-small'">
+            <b-icon-arrow-bar-left></b-icon-arrow-bar-left>
+          </b-button>
+        </div>
+      </template>
+      <div class="sidebar-header">
+        <img alt="Vue logo" src="@/assets/logo.png" width="32" height="32" />
+        <h3>Yacht</h3>
+        <p>Container Managment UI</p>
+      </div>
+    </b-sidebar>
+    <b-sidebar
+      id="sidebar-small"
+      width="5em"
+      shadow
+      no-header-close
+      no-close-on-esc
+      no-close-on-route-change
+      visible
+    >
+      <template v-slot:footer>
+        <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
+          <b-button squared v-b-toggle="'sidebar-big sidebar-small'">
+            <b-icon-arrow-bar-right></b-icon-arrow-bar-right>
+          </b-button>
+        </div>
+      </template>
+      <div class="sidebar-header">
+        <img alt="Vue logo" src="@/assets/logo.png" width="32" height="32" />
+      </div>
+    </b-sidebar>
+  </div>
+</template>
