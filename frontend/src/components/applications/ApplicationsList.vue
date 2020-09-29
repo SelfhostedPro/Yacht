1d920f5d7e350d2d6c4c317f3b982a63f2fd2cd1
SelfhostedPro <info@selfhosted.pro>
2020-09-29 14:23:26 -0700

    added updated button

--- frontend/src/components/applications/ApplicationsList.vue
+++ frontend/src/components/applications/ApplicationsList.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="apps-list component">
+  <div class="apps-list component" style="max-width: 90%">
     <v-card>
       <v-fade-transition>
         <v-progress-linear
@@ -21,6 +21,8 @@
         ></v-text-field>
       </v-card-title>
       <v-data-table
+        style="width: 99%"
+        class="mx-auto"
         :headers="headers"
         :items="apps"
         :items-per-page="10"
@@ -63,6 +65,15 @@
                 </v-list-item>
                 <v-divider />
                 <v-list-item
+                  @click="AppAction({ Name: item.name, Action: 'update' })"
+                >
+                  <v-list-item-icon>
+                    <v-icon>mdi-update</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-title>Update</v-list-item-title>
+                </v-list-item>
+                <v-divider />
+                <v-list-item
                   @click="AppAction({ Name: item.name, Action: 'kill' })"
                 >
                   <v-list-item-icon>
@@ -90,46 +101,35 @@
         </template>
         <template v-slot:item.ports="{ item }">
           <ins v-for="(port, index) in convPorts(item.ports)" :key="index">
-            <v-tooltip top transition="scale-transition">
-              <template v-slot:activator="{ on, attrs }">
-                <v-chip
-                  v-on="on"
-                  v-bind="attrs"
-                  class="mx-1"
-                  v-if="port.hip == '0.0.0.0'"
-                  color="indigo darken-2"
-                  label
-                  small
-                  :href="'http://' + host_ip + ':' + port.hport"
-                  target="_blank"
-                  ><v-icon small class="mr-1">mdi-link-variant</v-icon
-                  >{{
-                    item.Config.Labels[`local.yacht.port.${port.hport}`] ||
-                      port.hport
-                  }}</v-chip
-                >
-                <v-chip
-                  v-on="on"
-                  v-bind="attrs"
-                  class="ma-1"
-                  v-else
-                  color="indigo darken-2"
-                  label
-                  small
-                  :href="'http://' + port.hip + ':' + port.hport"
-                  target="_blank"
-                  ><v-icon small class="mr-1">mdi-link-variant</v-icon
-                  >{{ item.Config.Labels || port.hport }}</v-chip
-                >
-              </template>
-              <span>
-                {{ port.hport }}
-              </span>
-            </v-tooltip>
+            <v-chip
+              class="mx-1"
+              v-if="port.hip == '0.0.0.0'"
+              color="indigo darken-2"
+              label
+              small
+              :href="'http://' + host_ip + ':' + port.hport"
+              target="_blank"
+              ><v-icon small class="mr-1">mdi-link-variant</v-icon
+              >{{ port.hport }}</v-chip
+            >
+            <v-chip
+              class="ma-1"
+              v-else
+              color="indigo darken-2"
+              label
+              small
+              :href="'http://' + port.hip + ':' + port.hport"
+              target="_blank"
+              ><v-icon small class="mr-1">mdi-link-variant</v-icon
+              >{{ port.hport }}</v-chip
+            >
           </ins>
         </template>
         <template v-slot:item.image="{ item }">
           <span class="ImageName">{{ item.Config.Image }}</span>
+        </template>
+        <template v-slot:item.created="{item}">
+          <span class="CreatedAt"> {{ item.Created | formatDate }} </span>
         </template>
       </v-data-table>
     </v-card>
@@ -150,32 +150,36 @@ export default {
           value: "name",
           sortable: true,
           align: "start",
-          width: "30%"
+          width: "30%",
         },
         {
           text: "Status",
           value: "status",
           sortable: true,
-          width: "10%"
+          width: "10%",
         },
         {
           text: "Image",
           value: "image",
-          sortable: true
+          sortable: true,
         },
         {
           text: "Ports",
           value: "ports",
           sortable: true,
-          width: "30%"
+        },
+        {
+          text: "Created At",
+          value: "created",
+          sortable: true,
         }
-      ]
+      ],
     };
   },
   methods: {
     ...mapActions({
       readApps: "apps/readApps",
-      AppAction: "apps/AppAction"
+      AppAction: "apps/AppAction",
     }),
     handleRowClick(appName) {
       this.$router.push({ path: `/apps${appName.Name}/info` });
@@ -195,14 +199,14 @@ export default {
     },
     refresh() {
       this.readApps();
-    }
+    },
   },
   computed: {
-    ...mapState("apps", ["apps", "isLoading"])
+    ...mapState("apps", ["apps", "isLoading"]),
   },
   mounted() {
     this.readApps();
-  }
+  },
 };
 </script>
 
@@ -211,6 +215,11 @@ tr:hover {
   cursor: pointer;
 }
 .ImageName {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.CreatedAt{
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
