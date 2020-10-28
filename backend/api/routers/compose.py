d3a3d37786d965f421303011905b44e403e00ad0
SelfhostedPro <info@selfhosted.pro>
2020-10-28 09:36:18 -0700

    building frontend out for projects

--- backend/api/routers/compose.py
+++ backend/api/routers/compose.py
@@ -5,14 +5,14 @@ from ..actions.compose import get_compose_projects, compose_action, get_compose
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/", dependencies=[Depends(get_active_user)])
 def get_projects():
     return get_compose_projects()
 
-@router.get("/{project_name}")
+@router.get("/{project_name}", dependencies=[Depends(get_active_user)])
 def get_project(project_name):
     return get_compose(project_name)
 
-@router.get("/{project_name}/{action}")
+@router.get("/{project_name}/{action}", dependencies=[Depends(get_active_user)])
 def get_compose_action(project_name, action):
     return compose_action(project_name, action)
