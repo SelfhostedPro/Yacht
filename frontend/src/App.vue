bd64a73959c45dd0ddf67017ead1bb489ca402af
SelfhostedPro <info@selfhosted.pro>
2021-02-21 20:02:46 -0800

    working on fixing theme being unset after auth timeout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -45,25 +45,36 @@ export default {
     Sidebar: Sidebar,
     Appbar: Appbar,
     LoginForm: LoginForm,
-    snackbar: snackbar
+    snackbar: snackbar,
   },
   data: () => ({}),
   computed: {
     ...mapGetters({
       isLoggedIn: "auth/isAuthenticated",
-      authDisabled: "auth/authDisabled"
+      authDisabled: "auth/authDisabled",
     }),
     theme() {
       return this.$vuetify.theme.dark ? "dark" : "light";
-    }
+    },
   },
   methods: {
     ...mapActions({
-      authCheck: "auth/AUTH_CHECK"
-    })
+      authCheck: "auth/AUTH_CHECK",
+    }),
   },
   created() {
     this.authCheck();
+    const dark_theme = localStorage.getItem("dark_theme");
+    const theme = JSON.parse(localStorage.getItem("theme"));
+
+    if (dark_theme == "false") {
+      this.$vuetify.theme.dark = false;
+    } else if (dark_theme == "true") {
+      this.$vuetify.theme.dark = true;
+    }
+    if (theme) {
+      this.$vuetify.theme.themes = theme;
+    }
   },
   mounted() {
     const dark_theme = localStorage.getItem("dark_theme");
@@ -77,7 +88,7 @@ export default {
     if (theme) {
       this.$vuetify.theme.themes = theme;
     }
-  }
+  },
 };
 </script>
 
