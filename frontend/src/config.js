5f34c5611eb599c006e8f23ad90dcaacb6a04439
SelfhostedPro <info@selfhosted.pro>
2021-03-02 10:45:28 -0800

    initial react

--- frontend/src/config.js
+++
@@ -1,7 +0,0 @@
-export const themeTheme = process.env.VUE_APP_THEME;
-export const themeLogo = process.env.VUE_APP_LOGO;
-
-export default {
-  themeTheme,
-  themeLogo
-};
--- frontend/src/views/Container.vue
+++
@@ -1,9 +0,0 @@
-<template lang="html">
-  <h1>Container</h1>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style lang="css" scoped></style>
