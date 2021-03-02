cf1770d9ebcef50a4b6fcb6fe6b4955fa9e9dfa0
SelfhostedPro <info@selfhosted.pro>
2021-03-02 14:45:35 -0800

    added reverseproxy to frontend development server

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,10 +1,24 @@
 module.exports = {
     style: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
+        postcss: {
+            plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
     },
-  }
+    devServer: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:8000",
+                timeout: 6000,
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    "^/api": ""
+                },
+                logLevel: "debug"
+            }
+        }
+    }
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { RecoilRoot } from 'recoil';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RecoilRoot>
+    </RecoilRoot>
+  )
 }
 
 export default App;
