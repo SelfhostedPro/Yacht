f8aa52c394a3b841c43732a31acc28072db09aa7
Marc Miller <marcjmiller@outlook.com>
2021-03-02 23:02:48 -0500

    makes a table now

--- frontend/src/components/AppsTable.tsx
+++ frontend/src/components/AppsTable.tsx
@@ -1,18 +1,32 @@
 import { useEffect } from "react";
-import { atom, useRecoilState, useRecoilValue } from "recoil";
+import { atom, useRecoilState } from "recoil";
 
 const appsState = atom({
-  key: 'AppsState',
-  default: [] // TODO:  We really should apply types here later, and possibly move the Atoms into a consolidated location/file
+  key: "AppsState",
+  default: [] as App[], // TODO:  We really should apply types here later, and possibly move the Atoms into a consolidated location/file
 });
-const AppsList = () => {
+
+interface App {
+  name: string;
+  short_id: string;
+  ports: {}; // {[name: string]: [{HostIp: string, HostPort: string}]} // something like this?
+}
+
+// interface Port {
+//   HostIp: string;
+//   HostPort: string;
+// }
+
+const AppsTable = () => {
   const [apps, setApps] = useRecoilState(appsState);
 
   useEffect(() => {
     const fetchApps = async () => {
       await fetch("/api/apps/")
         .then((res) => res.json())
-        .then((data) => setApps(data));
+        .then((data) => {
+          setApps(data);
+        });
     };
 
     fetchApps();
@@ -21,19 +35,22 @@ const AppsList = () => {
   return (
     <div>
       <div>Hello world, we be fetching apps!</div>
-      <div className="text-xs">{JSON.stringify(apps)}</div>
+      <div className={"flex flex-row justify-between w-1/4"}>
+        <div>name</div>
+        <div>short_id</div>
+        {/* <div>ports</div> */}
+      </div>
+      {apps.map((app, index) => {
+        return (
+          <div className={"flex flex-row justify-between w-1/4"} key={index}>
+            <div>{app.name}</div>
+            <div>{app.short_id}</div>
+            {/* <div>{app.ports}</div> */}
+          </div>
+        );
+      })}
     </div>
   );
-};
-
-const AppsTable = () => {
-  const Apps = useRecoilValue(appsState);
-
-  return (
-    <div>
-      {Apps.map(app => { return (<tbody>{app}</tbody>) })}
-    </div>
-  )
 };
 
 export default AppsTable;
