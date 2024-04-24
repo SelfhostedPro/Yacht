c463a22a13f7dbf619af7c8f457b5240cb49ddd0
SelfhostedPro <info@selfhosted.pro>
2024-04-24 12:31:40 -0700

    removing folder creation from config plugin

--- modules/config/plugin/server.ts
+++ modules/config/plugin/server.ts
@@ -5,14 +5,6 @@ import fs from 'fs-extra'
 // Loading the config on initialization so we can make sure settings are applied on startup.
 export default defineNitroPlugin(async (nitroApp) => {
     Logger.info('initializing config module...', 'config')
-    fs.ensureDir(useRuntimeConfig().yacht.configOptions.configPath, (err) => {
-        if (err) Logger.error(err)
-        else Logger.info(`Creating config directory!`, 'config - plugin')
-    })
-    fs.ensureDir(useRuntimeConfig().yacht.configOptions.dataPath, (err) => {
-        if (err) Logger.error(err)
-        else Logger.info(`Creating data directory!`, 'config - plugin')
-    })
     await checkConfig()
     Logger.ready('config module initialized.', 'config')
 })
